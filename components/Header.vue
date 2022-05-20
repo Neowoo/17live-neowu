f09768dc4d6f37d71dee6a8a6b717e15d99f8412
Neowu2021 <wuhongyi2011@gmail.com>
2022-05-20 18:40:35 +0800

    init project

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-
+  <div class="header">
+    <img src="/images/burger-icon.svg" width="32" height="24.5" alt="">
+    
   </div>
 </template>
+<script>
+
+</script>
+<style lang="scss">
+  .header {
+    background-color: #8DC8FF;
+    width: 100%;
+    height: 55px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+<style lang="scss">
+* {
+  margin: 0;
+}
+</style>
